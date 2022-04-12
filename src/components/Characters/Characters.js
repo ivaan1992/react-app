c4e8a5cb81f4aa49183d1173c0fab57fc0ddab0a
ivaan1992 <ivaan.lg.92@gmail.com>
2022-04-12 11:27:10 -0500

    Remove components original, and add new ones, and add routes

---
+++ src/components/Characters/Characters.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Characters = () => (
+  <div>
+    <h1>characters section</h1>
+  </div>
+);
+
+export default Characters;
---
+++ src/components/Episodes/Episodes.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Episodes = () => (
+  <div>
+    <h1>Episodes section</h1>
+  </div>
+);
+
+export default Episodes;
