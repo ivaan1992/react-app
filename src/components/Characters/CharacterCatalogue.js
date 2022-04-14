db5263fd9042104e84f2d975585f27ed543066cf
ivaan1992 <ivaan.lg.92@gmail.com>
2022-04-13 21:07:48 -0500

    Finish the catalogue chars

---
+++ src/components/Characters/CharacterCatalogue.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Characters.css';
+
+const CharacterCatalogue = ({
+  img, name, status, location,
+}) => (
+  <div className="d-flex card-catalogue">
+    <img src={img} alt={name} />
+    <div className="info-character-catalogue">
+      <h1 className="character-name-cat">{name}</h1>
+      <div className="status-red">{status}</div>
+      {/*
+
+        () => (
+            if (status === 'Dead') {
+              return (
+                <div className="status-red">{status}</div>
+              );
+            } if (status === 'Alive') {
+              return (
+                <div className="status-green">{status}</div>
+              );
+            } if (status === 'Unknown') {
+              return (
+                <div className="status-gray">{status}</div>
+              );
+            }
+        )
+      */ }
+      <h3>{location.name}</h3>
+    </div>
+  </div>
+);
+
+CharacterCatalogue.propTypes = {
+  img: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+};
+
+export default CharacterCatalogue;
--- src/components/Characters/CharacterInfo.js
+++ src/components/Characters/CharacterInfo.js
@@ -1,4 +1,4 @@
-import React from 'react';
+/* import React from 'react';
 import PropTypes from 'prop-types';
 
 const CharacterInfo = ({
@@ -27,4 +27,4 @@ CharacterInfo.propTypes = {
   type: PropTypes.string.isRequired,
 };
 
-export default CharacterInfo;
+export default CharacterInfo; */
