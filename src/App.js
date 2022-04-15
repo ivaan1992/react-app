8dcd4b4982d79159d82c3bc4c6ed3bf4cc3ade75
ivaan1992 <ivaan.lg.92@gmail.com>
2022-04-15 15:13:20 -0500

    Now you can see card info complete when you click on the characters

--- src/App.js
+++ src/App.js
@@ -3,13 +3,16 @@ import './App.css';
 import Header from './components/Header/Header';
 import Characters from './components/Characters/Characters';
 import Episodes from './components/Episodes/Episodes';
+import Details from './components/Characters/Details';
 
 const App = () => (
   <BrowserRouter>
     <Header />
     <Routes>
       <Route path="/" element={<Characters />} />
+      <Route path="/:id" element={<Details />} />
       <Route path="/Episodes" element={<Episodes />} />
+      <Route path="/Episodes/:id" element={<Details />} />
     </Routes>
   </BrowserRouter>
 );
--- src/redux/Characters/CharactersRedux.js
+++ src/redux/Characters/CharactersRedux.js
@@ -2,10 +2,12 @@ import connectCharApi from '../Api/characterApi';
 
 const CHARACTERS = 'redux/actions/get_characters';
 const NAME = 'redux/actions/get_characters/name';
+const DETAILS = 'redux/actions/get_characters/details';
 
 const initialState = {
   name: '',
   chars: [],
+  details: {},
 };
 
 const characters = (character, type) => ({
@@ -13,11 +15,17 @@ const characters = (character, type) => ({
   payload: character,
 });
 
-export const getCharacter = ({ page, dispatch, name = '' }) => {
+export const getCharacter = ({
+  page, dispatch, name = '',
+}) => {
   connectCharApi(page, name).then(({ results }) => {
     dispatch(characters(name, NAME));
     dispatch(characters(results, CHARACTERS));
   }).catch(() => {});
+};
+
+export const setSelectedCharacter = ({ character, dispatch }) => {
+  dispatch(characters(character, DETAILS));
 };
 
 /* export const getName = (name, dispatch) => {
@@ -32,6 +40,8 @@ const characterReducer = (state = initialState, action) => {
       return { ...state, chars: action.payload };
     case NAME:
       return { ...state, name: action.payload };
+    case DETAILS:
+      return { ...state, details: action.payload };
     default:
       return state;
   }
